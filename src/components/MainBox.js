f63ed2d287daa792e26d59e77105aa0e641ec48e
Andrew McIndoe <amcindoe@powerhrg.com>
2021-07-08 10:24:42 -0400

    Conditional Rendering Pages

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,25 +1,32 @@
-import React from "react";
-import MenuBar from "./MenuBar";
-import { Profile, Photos, Cocktails, Pokemon } from "./pages";
+import React, {useState} from "react"
+import MenuBar from "./MenuBar"
+import { Profile, Photos, Cocktails, Pokemon } from "./pages"
 
 function MainBox() {
-  /*
-    Replace the code below! Depending on what menu item is selected in the menu, 
-    I should render either a Profile, Photos, Cocktails, or Pokemon component.
-    Think of a way to track which menu item was selected. 
-    - Which component should have state? 
-    - Which component should have methods to control state? 
-    - Where should these methods be called?
-  */
 
-  let detailsToDisplay = <div>Hi, I'm a div!</div>;
+  const[MenuItem, SetMenuItem] = useState('Profile')
+
+
+  let componentToDisplay= 
+  <div>Just Smile and Wave.</div>
+
+  if (MenuItem === "Profile") {
+    componentToDisplay = <Profile />;
+  } else if (MenuItem === "Photos") {
+    componentToDisplay = <Photos />;
+  }
+  else if (MenuItem === "Cocktails") {
+    componentToDisplay = <Cocktails />;
+  }else if (MenuItem === "Pokemon") {
+    componentToDisplay = <Pokemon />
+  }
 
   return (
     <div>
-      <MenuBar />
-      {detailsToDisplay}
+      <MenuBar MenuItem={MenuItem} onClick={SetMenuItem}/>
+    {componentToDisplay}
     </div>
   );
 }
 
-export default MainBox;
+export default MainBox
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,36 +1,37 @@
-import React from "react";
+import React from "react"
 
-function MenuBar(props) {
-  /*
+  const MenuBar = ({ MenuItem, onClick }) => {
+    return (
+      <div className="ui four item menu">
+        <span
+          className={MenuItem === "Profile" ? "item active" : "item"}
+          onClick={() => onClick("Profile")}
+        >
+          <i className="user large icon" />
+        </span>
+  
+        <span
+          className={MenuItem === "Photo" ? "item active" : "item"}
+          onClick={() => onClick("Photos")}
+        >
+          <i className="photo large icon" />
+        </span>
+  
+        <span
+          className={MenuItem === "Cocktail" ? "item active" : "item"}
+          onClick={() => onClick("Cocktails")}
+        >
+          <i className="cocktail large icon" />
+        </span>
+  
+        <span
+          className={MenuItem === "Pokemon" ? "item active" : "item"}
+          onClick={() => onClick("Pokemon")}
+        >
+          <i className="themeisle large icon" />
+        </span>
+      </div>
+    );
+  }
 
-  The 'span' tags below are the menu items. Think about the way a menu 
-  should work. When you click a menu item, the button typically becomes
-  'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
-
-  */
-
-  return (
-    <div className="ui four item menu">
-      <span className="item active">
-        <i className="user large icon" />
-      </span>
-
-      <span className="item">
-        <i className="photo large icon" />
-      </span>
-
-      <span className="item">
-        <i className="cocktail large icon" />
-      </span>
-
-      <span className="item">
-        <i className=" themeisle large icon" />
-      </span>
-    </div>
-  );
-}
-
-export default MenuBar;
+export default MenuBar
